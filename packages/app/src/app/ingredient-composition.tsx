8875c665df47fca2ebfc91d61dba7fe453db50e8
Ramon Sibello <ramon@sibello.ca>
2025-11-18 16:35:34 -0500

    Add top-level div to IngCompGrid for upcoming diff

--- packages/app/src/app/ingredient-composition.tsx
+++ packages/app/src/app/ingredient-composition.tsx
@@ -22,48 +22,50 @@ export function IngredientCompositionGrid({
   };
 
   return (
-    <div className="w-full min-w-[400px] overflow-x-auto whitespace-nowrap border-2 border-gray-400">
-      <table className="border-collapse">
-        {/* Header */}
-        <thead>
-          {/* Composition Header */}
-          <tr key={0} className="h-[24px] table-header-footer text-center">
-            {getTsEnumStringKeys(FlatHeader).map((header) => (
-              <th
-                key={header}
-                className="px-1 w-fit border-b-1 border-gray-400 border-r border-gray-300"
-              >
-                {flat_header_as_med_str_js(header)}
-              </th>
-            ))}
-          </tr>
-          {/* Totals Row */}
-          <tr className="h-[25px] table-header-footer">
-            {getTsEnumStringKeys(FlatHeader).map((header) => (
-              <td
-                key={header}
-                className="text-center border-b border-gray-400 border-r border-gray-300"
-              ></td>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {/* Composition Rows */}
-          {/* @todo The very last row is a little taller than the rest; not sure why */}
-          {recipeState.map((_, index) => (
-            <tr key={index} className="h-[25px] border-b border-gray-300">
+    <div>
+      <div className="w-full min-w-[400px] overflow-x-auto whitespace-nowrap border-2 border-gray-400">
+        <table className="border-collapse">
+          {/* Header */}
+          <thead>
+            {/* Composition Header */}
+            <tr key={0} className="h-[24px] table-header-footer text-center">
+              {getTsEnumStringKeys(FlatHeader).map((header) => (
+                <th
+                  key={header}
+                  className="px-1 w-fit border-b-1 border-gray-400 border-r border-gray-300"
+                >
+                  {flat_header_as_med_str_js(header)}
+                </th>
+              ))}
+            </tr>
+            {/* Totals Row */}
+            <tr className="h-[25px] table-header-footer">
               {getTsEnumStringKeys(FlatHeader).map((header) => (
                 <td
                   key={header}
-                  className="border-r border-gray-300 text-sm text-gray-900 text-center"
-                >
-                  {formattedCompCell(index, header)}
-                </td>
+                  className="text-center border-b border-gray-400 border-r border-gray-300"
+                ></td>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {/* Composition Rows */}
+            {/* @todo The very last row is a little taller than the rest; not sure why */}
+            {recipeState.map((_, index) => (
+              <tr key={index} className="h-[25px] border-b border-gray-300">
+                {getTsEnumStringKeys(FlatHeader).map((header) => (
+                  <td
+                    key={header}
+                    className="border-r border-gray-300 text-sm text-gray-900 text-center"
+                  >
+                    {formattedCompCell(index, header)}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
