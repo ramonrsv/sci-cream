ramonrsv/sci-cream

commit 8875c665df47fca2ebfc91d61dba7fe453db50e8

Add top-level div to IngCompGrid for upcoming diff

packages/app/src/app/ingredient-composition.tsx
@@ -22,48 +22,50 @@ export function IngredientCompositionGrid({
   };
 
   return (
-    <div className="w-full min-w-[400px] overflow-x-auto whitespace-nowrap border-2 border-gray-400">
-      <table className="border-collapse">
-        {/* Header */}
-        <thead>
-          {/* Composition Header */}
-          <tr key={0} className="h-[24px] table-header-footer text-center">
-            {getTsEnumStringKeys(FlatHeader).map((header) => (
-              <th
-                key={header}
-                className="px-1 w-fit border-b-1 border-gray-400 border-r border-gray-300"
-              >
-                {flat_header_as_med_str_js(header)}
-              </th>
-            ))}
-          </tr>
-          {/* Totals Row */}
-          <tr className="h-[25px] table-header-footer">
-            {getTsEnumStringKeys(FlatHeader).map((header) => (
-              <td
-                key={header}
-                className="text-center border-b border-gray-400 border-r border-gray-300"
-              ></td>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {/* Composition Rows */}
-          {/* @todo The very last row is a little taller than the rest; not sure why */}
-          {recipeState.map((_, index) => (
-            <tr key={index} className="h-[25px] border-b border-gray-300">
+    <div>
+      <div className="w-full min-w-[400px] overflow-x-auto whitespace-nowrap border-2 border-gray-400">
+        <table className="border-collapse">
+          {/* Header */}
+          <thead>
+            {/* Composition Header */}
+            <tr key={0} className="h-[24px] table-header-footer text-center">
+              {getTsEnumStringKeys(FlatHeader).map((header) => (
+                <th
+                  key={header}
+                  className="px-1 w-fit border-b-1 border-gray-400 border-r border-gray-300"
+                >
+                  {flat_header_as_med_str_js(header)}
+                </th>
+              ))}
+            </tr>
+            {/* Totals Row */}
+            <tr className="h-[25px] table-header-footer">
               {getTsEnumStringKeys(FlatHeader).map((header) => (
                 <td
                   key={header}
-                  className="border-r border-gray-300 text-sm text-gray-900 text-center"
-                >
-                  {formattedCompCell(index, header)}
-                </td>
+                  className="text-center border-b border-gray-400 border-r border-gray-300"
+                ></td>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {/* Composition Rows */}
+            {/* @todo The very last row is a little taller than the rest; not sure why */}
+            {recipeState.map((_, index) => (
+              <tr key={index} className="h-[25px] border-b border-gray-300">
+                {getTsEnumStringKeys(FlatHeader).map((header) => (
+                  <td
+                    key={header}
+                    className="border-r border-gray-300 text-sm text-gray-900 text-center"
+                  >
+                    {formattedCompCell(index, header)}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }

commit 548ca48961c97958296028ed9b8355662164b7fc

Fix CompGrid scrolling that was broken in 8875c66
Horizontal scroling of `IngredientCompositionGrid` was broken in
8875c66, "Add top-level div to IngCompGrid for upcoming diff". Fix
the broken scrolling by moving the width settings to the top-level
<div> and the overflow settings to the <div> wrapping <table>.

@todo There is still one issue where the border around the top and sides
of the header rows is too thick because there are both an outer and inner
border that join together.

packages/app/src/app/ingredient-composition.tsx
@@ -43,7 +43,7 @@ export function IngredientCompositionGrid({ recipeState }: { recipeState: Recipe
   };
 
   return (
-    <div>
+    <div className="w-full min-w-[200px]">
       <select
         className="border-gray-400 border text-gray-900 text-sm"
         value={qtyToggle}
@@ -53,8 +53,8 @@ export function IngredientCompositionGrid({ recipeState }: { recipeState: Recipe
         <option value={QtyToggle.Quantity}>{QtyToggle.Quantity}</option>
         <option value={QtyToggle.Percentage}>{QtyToggle.Percentage}</option>
       </select>
-      <div className="w-full min-w-[200px] overflow-x-auto whitespace-nowrap">
-        <table className="border-collapse border-gray-400 border-2">
+      <div className="border-collapse border-gray-400 border-2 overflow-x-auto whitespace-nowrap">
+        <table>
           {/* Header */}
           <thead>
             {/* Composition Header */}