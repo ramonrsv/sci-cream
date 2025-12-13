7ddc57e99362cf67dd14e4f384146613d2dfe8c1
Ramon Sibello <ramon@sibello.ca>
2025-12-14 01:13:54 +0800

    Fix broken composition.test.ts after -unspecified

--- packages/sci-cream/src/ts/composition.test.ts
+++ packages/sci-cream/src/ts/composition.test.ts
@@ -8,31 +8,22 @@ function new_sugars_sucrose(amount: number): Sugars {
   return sugars;
 }
 
-function new_sugars_unspecified(amount: number): Sugars {
-  const sugars = new Sugars();
-  sugars.unspecified = amount;
-  return sugars;
-}
-
-test("Sugars.to_pod_js", () => {
-  expect(new_sugars_sucrose(10).to_pod_js()).toBe(10);
-  expect(new_sugars_unspecified(10).to_pod_js()).toBeUndefined();
+test("Sugars.to_pod", () => {
+  expect(new_sugars_sucrose(10).to_pod()).toBe(10);
 });
 
-test("Sugars.to_pac_js", () => {
-  expect(new_sugars_sucrose(10).to_pac_js()).toBe(10);
-  expect(new_sugars_unspecified(10).to_pac_js()).toBeUndefined();
+test("Sugars.to_pac", () => {
+  expect(new_sugars_sucrose(10).to_pac()).toBe(10);
 });
 
 test("Sweeteners.to_pod_js", () => {
   const sweeteners = new Sweeteners();
 
   sweeteners.sugars = new_sugars_sucrose(10);
-  expect(sweeteners.sugars.to_pod_js()).toBe(10);
+  expect(sweeteners.sugars.to_pod()).toBe(10);
   expect(sweeteners.to_pod_js()).toBe(10);
 
-  sweeteners.sugars = new_sugars_unspecified(10);
-  expect(sweeteners.sugars.to_pod_js()).toBeUndefined();
+  sweeteners.artificial = 10;
   expect(sweeteners.to_pod_js()).toBeUndefined();
 });
 
@@ -40,10 +31,9 @@ test("Sweeteners.to_pac_js", () => {
   const sweeteners = new Sweeteners();
 
   sweeteners.sugars = new_sugars_sucrose(10);
-  expect(sweeteners.sugars.to_pac_js()).toBe(10);
+  expect(sweeteners.sugars.to_pac()).toBe(10);
   expect(sweeteners.to_pac_js()).toBe(10);
 
-  sweeteners.sugars = new_sugars_unspecified(10);
-  expect(sweeteners.sugars.to_pac_js()).toBeUndefined();
+  sweeteners.artificial = 10;
   expect(sweeteners.to_pac_js()).toBeUndefined();
 });
