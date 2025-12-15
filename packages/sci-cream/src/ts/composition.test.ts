a306e2c6537b945f5486ff80510e2a73afa0ce34
Ramon Sibello <ramon@sibello.ca>
2025-12-15 18:26:48 -0500

    Revert "Fix broken composition.test.ts after -unspecified"
    This reverts commit 7ddc57e99362cf67dd14e4f384146613d2dfe8c1.

--- packages/sci-cream/src/ts/composition.test.ts
+++ packages/sci-cream/src/ts/composition.test.ts
@@ -8,22 +8,31 @@ function new_sugars_sucrose(amount: number): Sugars {
   return sugars;
 }
 
-test("Sugars.to_pod", () => {
-  expect(new_sugars_sucrose(10).to_pod()).toBe(10);
+function new_sugars_unspecified(amount: number): Sugars {
+  const sugars = new Sugars();
+  sugars.unspecified = amount;
+  return sugars;
+}
+
+test("Sugars.to_pod_js", () => {
+  expect(new_sugars_sucrose(10).to_pod_js()).toBe(10);
+  expect(new_sugars_unspecified(10).to_pod_js()).toBeUndefined();
 });
 
-test("Sugars.to_pac", () => {
-  expect(new_sugars_sucrose(10).to_pac()).toBe(10);
+test("Sugars.to_pac_js", () => {
+  expect(new_sugars_sucrose(10).to_pac_js()).toBe(10);
+  expect(new_sugars_unspecified(10).to_pac_js()).toBeUndefined();
 });
 
 test("Sweeteners.to_pod_js", () => {
   const sweeteners = new Sweeteners();
 
   sweeteners.sugars = new_sugars_sucrose(10);
-  expect(sweeteners.sugars.to_pod()).toBe(10);
+  expect(sweeteners.sugars.to_pod_js()).toBe(10);
   expect(sweeteners.to_pod_js()).toBe(10);
 
-  sweeteners.artificial = 10;
+  sweeteners.sugars = new_sugars_unspecified(10);
+  expect(sweeteners.sugars.to_pod_js()).toBeUndefined();
   expect(sweeteners.to_pod_js()).toBeUndefined();
 });
 
@@ -31,9 +40,10 @@ test("Sweeteners.to_pac_js", () => {
   const sweeteners = new Sweeteners();
 
   sweeteners.sugars = new_sugars_sucrose(10);
-  expect(sweeteners.sugars.to_pac()).toBe(10);
+  expect(sweeteners.sugars.to_pac_js()).toBe(10);
   expect(sweeteners.to_pac_js()).toBe(10);
 
-  sweeteners.artificial = 10;
+  sweeteners.sugars = new_sugars_unspecified(10);
+  expect(sweeteners.sugars.to_pac_js()).toBeUndefined();
   expect(sweeteners.to_pac_js()).toBeUndefined();
 });
