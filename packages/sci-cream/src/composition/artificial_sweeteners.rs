ramonrsv/sci-cream

commit 472dc461f3fa1f59863ac0e46a26972a56c14228

Pull each composition struct into its own file
Introduce `/src/composition` directory for the `composition` module, and
move each struct from the former `composition.rs` into its own file. It
was already getting cumbersome to move around that large file, and that
was before adding all the documentation and unit tests.

packages/sci-cream/src/composition/artificial_sweeteners.rs
@@ -1,0 +1,135 @@
+use approx::AbsDiffEq;
+use serde::{Deserialize, Serialize};
+use struct_iterable::Iterable;
+
+use crate::{
+    composition::ScaleComponents,
+    constants,
+    error::{Error, Result},
+    util::{iter_all_abs_diff_eq, iter_fields_as},
+};
+
+#[cfg(feature = "wasm")]
+use wasm_bindgen::prelude::*;
+
+/// Non-saccharide artificial sweeteners, commonly used as sugar substitutes, e.g. aspartame
+///
+/// **Note**: These are distinct from sugar alcohols (e.g. erythritol, maltitol, etc.) which are
+/// not used in similar quantities. See [`Polyols`].
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
+#[derive(Iterable, PartialEq, Serialize, Deserialize, Copy, Clone, Debug)]
+#[serde(default, deny_unknown_fields)]
+pub struct ArtificialSweeteners {
+    pub aspartame: f64,
+    pub saccharin: f64,
+    pub sucralose: f64,
+    pub other: f64,
+}
+
+impl ArtificialSweeteners {
+    pub fn empty() -> Self {
+        Self {
+            aspartame: 0.0,
+            saccharin: 0.0,
+            sucralose: 0.0,
+
+            other: 0.0,
+        }
+    }
+
+    pub fn aspartame(self, aspartame: f64) -> Self {
+        Self { aspartame, ..self }
+    }
+
+    pub fn saccharin(self, saccharin: f64) -> Self {
+        Self { saccharin, ..self }
+    }
+
+    pub fn sucralose(self, sucralose: f64) -> Self {
+        Self { sucralose, ..self }
+    }
+
+    pub fn other(self, other: f64) -> Self {
+        Self { other, ..self }
+    }
+
+    pub fn to_pod(&self) -> Result<f64> {
+        if self.other != 0.0 {
+            return Err(Error::CannotComputePOD("Other artificial sweeteners should be zero".to_string()));
+        }
+
+        Ok([
+            self.aspartame * constants::pod::ASPARTAME,
+            self.saccharin * constants::pod::SACCHARIN,
+            self.sucralose * constants::pod::SUCRALOSE,
+        ]
+        .into_iter()
+        .sum::<f64>()
+            / 100.0)
+    }
+
+    pub fn to_pac(&self) -> Result<f64> {
+        if self.other != 0.0 {
+            return Err(Error::CannotComputePAC("Other artificial sweeteners should be zero".to_string()));
+        }
+
+        Ok([
+            self.aspartame * constants::pac::ASPARTAME,
+            self.saccharin * constants::pac::SACCHARIN,
+            self.sucralose * constants::pac::SUCRALOSE,
+        ]
+        .into_iter()
+        .sum::<f64>()
+            / 100.0)
+    }
+}
+
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
+impl ArtificialSweeteners {
+    #[cfg_attr(feature = "wasm", wasm_bindgen(constructor))]
+    pub fn new() -> Self {
+        Self::empty()
+    }
+
+    pub fn total(&self) -> f64 {
+        iter_fields_as::<f64, _>(self).sum()
+    }
+}
+
+impl ScaleComponents for ArtificialSweeteners {
+    fn scale(&self, factor: f64) -> Self {
+        Self {
+            aspartame: self.aspartame * factor,
+            saccharin: self.saccharin * factor,
+            sucralose: self.sucralose * factor,
+            other: self.other * factor,
+        }
+    }
+
+    fn add(&self, other: &Self) -> Self {
+        Self {
+            aspartame: self.aspartame + other.aspartame,
+            saccharin: self.saccharin + other.saccharin,
+            sucralose: self.sucralose + other.sucralose,
+            other: self.other + other.other,
+        }
+    }
+}
+
+impl AbsDiffEq for ArtificialSweeteners {
+    type Epsilon = f64;
+
+    fn default_epsilon() -> Self::Epsilon {
+        f64::default_epsilon()
+    }
+
+    fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+        iter_all_abs_diff_eq::<f64, f64, Self>(self, other, epsilon)
+    }
+}
+
+impl Default for ArtificialSweeteners {
+    fn default() -> Self {
+        Self::empty()
+    }
+}

packages/sci-cream/src/composition/polyols.rs
@@ -1,0 +1,144 @@
+use approx::AbsDiffEq;
+use serde::{Deserialize, Serialize};
+use struct_iterable::Iterable;
+
+use crate::{
+    composition::ScaleComponents,
+    constants,
+    error::{Error, Result},
+    util::{iter_all_abs_diff_eq, iter_fields_as},
+};
+
+#[cfg(feature = "wasm")]
+use wasm_bindgen::prelude::*;
+
+/// Sugar alcohols, commonly used as sugar substitutes, e.g. erythritol, maltitol, etc.
+///
+/// **Note**: These are distinct from non-saccharide artificial sweeteners (e.g. aspartame,
+/// sucralose, etc.) which are not used in similar quantities. See [`ArtificialSweeteners`].
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
+#[derive(Iterable, PartialEq, Serialize, Deserialize, Copy, Clone, Debug)]
+#[serde(default, deny_unknown_fields)]
+pub struct Polyols {
+    pub erythritol: f64,
+    pub maltitol: f64,
+    pub sorbitol: f64,
+    pub xylitol: f64,
+    pub other: f64,
+}
+
+impl Polyols {
+    pub fn empty() -> Self {
+        Self {
+            erythritol: 0.0,
+            maltitol: 0.0,
+            sorbitol: 0.0,
+            xylitol: 0.0,
+            other: 0.0,
+        }
+    }
+
+    pub fn erythritol(self, erythritol: f64) -> Self {
+        Self { erythritol, ..self }
+    }
+
+    pub fn maltitol(self, maltitol: f64) -> Self {
+        Self { maltitol, ..self }
+    }
+
+    pub fn sorbitol(self, sorbitol: f64) -> Self {
+        Self { sorbitol, ..self }
+    }
+
+    pub fn xylitol(self, xylitol: f64) -> Self {
+        Self { xylitol, ..self }
+    }
+
+    pub fn other(self, other: f64) -> Self {
+        Self { other, ..self }
+    }
+
+    pub fn to_pac(&self) -> Result<f64> {
+        if self.other != 0.0 {
+            return Err(Error::CannotComputePAC("Other polyols should be zero".to_string()));
+        }
+
+        Ok([
+            self.erythritol * constants::pac::ERYTHRITOL,
+            self.maltitol * constants::pac::MALTITOL,
+            self.sorbitol * constants::pac::SORBITOL,
+            self.xylitol * constants::pac::XYLITOL,
+        ]
+        .into_iter()
+        .sum::<f64>()
+            / 100.0)
+    }
+
+    pub fn to_pod(&self) -> Result<f64> {
+        if self.other != 0.0 {
+            return Err(Error::CannotComputePOD("Other polyols should be zero".to_string()));
+        }
+
+        Ok([
+            self.erythritol * constants::pod::ERYTHRITOL,
+            self.maltitol * constants::pod::MALTITOL,
+            self.sorbitol * constants::pod::SORBITOL,
+            self.xylitol * constants::pod::XYLITOL,
+        ]
+        .into_iter()
+        .sum::<f64>()
+            / 100.0)
+    }
+}
+
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
+impl Polyols {
+    #[cfg_attr(feature = "wasm", wasm_bindgen(constructor))]
+    pub fn new() -> Self {
+        Self::empty()
+    }
+
+    pub fn total(&self) -> f64 {
+        iter_fields_as::<f64, _>(self).sum()
+    }
+}
+
+impl ScaleComponents for Polyols {
+    fn scale(&self, factor: f64) -> Self {
+        Self {
+            erythritol: self.erythritol * factor,
+            maltitol: self.maltitol * factor,
+            sorbitol: self.sorbitol * factor,
+            xylitol: self.xylitol * factor,
+            other: self.other * factor,
+        }
+    }
+
+    fn add(&self, other: &Self) -> Self {
+        Self {
+            erythritol: self.erythritol + other.erythritol,
+            maltitol: self.maltitol + other.maltitol,
+            sorbitol: self.sorbitol + other.sorbitol,
+            xylitol: self.xylitol + other.xylitol,
+            other: self.other + other.other,
+        }
+    }
+}
+
+impl AbsDiffEq for Polyols {
+    type Epsilon = f64;
+
+    fn default_epsilon() -> Self::Epsilon {
+        f64::default_epsilon()
+    }
+
+    fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+        iter_all_abs_diff_eq::<f64, f64, Self>(self, other, epsilon)
+    }
+}
+
+impl Default for Polyols {
+    fn default() -> Self {
+        Self::empty()
+    }
+}

packages/sci-cream/src/composition/alcohol.rs
@@ -1,0 +1,79 @@
+use approx::AbsDiffEq;
+use serde::{Deserialize, Serialize};
+use struct_iterable::Iterable;
+
+use crate::{composition::ScaleComponents, constants, util::iter_all_abs_diff_eq};
+
+#[cfg(feature = "wasm")]
+use wasm_bindgen::prelude::*;
+
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
+#[derive(Iterable, PartialEq, Serialize, Deserialize, Copy, Clone, Debug)]
+#[serde(default, deny_unknown_fields)]
+pub struct Alcohol {
+    pub by_weight: f64,
+}
+
+impl Alcohol {
+    pub fn empty() -> Self {
+        Self { by_weight: 0.0 }
+    }
+
+    pub fn by_weight(self, by_weight: f64) -> Self {
+        Self { by_weight }
+    }
+}
+
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
+impl Alcohol {
+    #[cfg_attr(feature = "wasm", wasm_bindgen(constructor))]
+    pub fn new() -> Self {
+        Self::empty()
+    }
+
+    pub fn from_abv(abv: f64) -> Self {
+        Self {
+            by_weight: abv * constants::ABV_TO_ABW_RATIO,
+        }
+    }
+
+    pub fn to_abv(&self) -> f64 {
+        self.by_weight / constants::ABV_TO_ABW_RATIO
+    }
+
+    pub fn to_pac(&self) -> f64 {
+        self.by_weight * constants::pac::ALCOHOL / 100.0
+    }
+}
+
+impl ScaleComponents for Alcohol {
+    fn scale(&self, factor: f64) -> Self {
+        Self {
+            by_weight: self.by_weight * factor,
+        }
+    }
+
+    fn add(&self, other: &Self) -> Self {
+        Self {
+            by_weight: self.by_weight + other.by_weight,
+        }
+    }
+}
+
+impl AbsDiffEq for Alcohol {
+    type Epsilon = f64;
+
+    fn default_epsilon() -> Self::Epsilon {
+        f64::default_epsilon()
+    }
+
+    fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+        iter_all_abs_diff_eq::<f64, f64, Self>(self, other, epsilon)
+    }
+}
+
+impl Default for Alcohol {
+    fn default() -> Self {
+        Self::empty()
+    }
+}

packages/sci-cream/src/composition/carbohydrates.rs
@@ -1,0 +1,130 @@
+use approx::AbsDiffEq;
+use serde::{Deserialize, Serialize};
+use struct_iterable::Iterable;
+
+use crate::{
+    composition::{Polyols, ScaleComponents, Sugars},
+    error::{Error, Result},
+};
+
+#[cfg(feature = "wasm")]
+use wasm_bindgen::prelude::*;
+
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
+#[derive(Iterable, PartialEq, Serialize, Deserialize, Copy, Clone, Debug)]
+#[serde(default, deny_unknown_fields)]
+pub struct Carbohydrates {
+    pub fiber: f64,
+    pub sugars: Sugars,
+    pub polyols: Polyols,
+    pub others: f64,
+}
+
+impl Carbohydrates {
+    pub fn empty() -> Self {
+        Self {
+            fiber: 0.0,
+            sugars: Sugars::empty(),
+            polyols: Polyols::empty(),
+            others: 0.0,
+        }
+    }
+
+    pub fn fiber(self, fiber: f64) -> Self {
+        Self { fiber, ..self }
+    }
+
+    pub fn sugars(self, sugars: Sugars) -> Self {
+        Self { sugars, ..self }
+    }
+
+    pub fn polyols(self, polyols: Polyols) -> Self {
+        Self { polyols, ..self }
+    }
+
+    pub fn others(self, others: f64) -> Self {
+        Self { others, ..self }
+    }
+
+    /// Sets `others = total - (fiber + sugars.total() + polyols.total())`
+    ///
+    /// # Errors
+    ///
+    /// Returns an [`Error::InvalidComposition`] if `total < (fiber + sugars.total() +
+    /// polyols.total())`; this should only be called once all other components have been set.
+    pub fn others_from_total(&self, total: f64) -> Result<Self> {
+        if (self.total() - self.others) > total {
+            return Err(Error::InvalidComposition(format!(
+                "Cannot set carbohydrate others from total: total {} is less than sum of other components {}",
+                total,
+                self.total() - self.others
+            )));
+        }
+
+        Ok(Self {
+            others: total - (self.total() - self.others),
+            ..*self
+        })
+    }
+
+    pub fn to_pod(&self) -> Result<f64> {
+        Ok(self.sugars.to_pod()? + self.polyols.to_pod()?)
+    }
+
+    pub fn to_pac(&self) -> Result<f64> {
+        Ok(self.sugars.to_pac()? + self.polyols.to_pac()?)
+    }
+}
+
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
+impl Carbohydrates {
+    #[cfg_attr(feature = "wasm", wasm_bindgen(constructor))]
+    pub fn new() -> Self {
+        Self::empty()
+    }
+
+    pub fn total(&self) -> f64 {
+        self.fiber + self.sugars.total() + self.polyols.total() + self.others
+    }
+}
+
+impl ScaleComponents for Carbohydrates {
+    fn scale(&self, factor: f64) -> Self {
+        Self {
+            fiber: self.fiber * factor,
+            sugars: self.sugars.scale(factor),
+            polyols: self.polyols.scale(factor),
+            others: self.others * factor,
+        }
+    }
+
+    fn add(&self, other: &Self) -> Self {
+        Self {
+            fiber: self.fiber + other.fiber,
+            sugars: self.sugars.add(&other.sugars),
+            polyols: self.polyols.add(&other.polyols),
+            others: self.others + other.others,
+        }
+    }
+}
+
+impl AbsDiffEq for Carbohydrates {
+    type Epsilon = f64;
+
+    fn default_epsilon() -> Self::Epsilon {
+        f64::default_epsilon()
+    }
+
+    fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+        self.fiber.abs_diff_eq(&other.fiber, epsilon)
+            && self.sugars.abs_diff_eq(&other.sugars, epsilon)
+            && self.polyols.abs_diff_eq(&other.polyols, epsilon)
+            && self.others.abs_diff_eq(&other.others, epsilon)
+    }
+}
+
+impl Default for Carbohydrates {
+    fn default() -> Self {
+        Self::empty()
+    }
+}

commit 9419d72903f158b8632a24d49e506c4096dd9b27

Fix documentation links broken in 472dc46

packages/sci-cream/src/composition/artificial_sweeteners.rs
@@ -12,6 +12,9 @@ use crate::{
 #[cfg(feature = "wasm")]
 use wasm_bindgen::prelude::*;
 
+#[cfg(doc)]
+use crate::composition::Polyols;
+
 /// Non-saccharide artificial sweeteners, commonly used as sugar substitutes, e.g. aspartame
 ///
 /// **Note**: These are distinct from sugar alcohols (e.g. erythritol, maltitol, etc.) which are

packages/sci-cream/src/composition/polyols.rs
@@ -12,6 +12,9 @@ use crate::{
 #[cfg(feature = "wasm")]
 use wasm_bindgen::prelude::*;
 
+#[cfg(doc)]
+use crate::composition::ArtificialSweeteners;
+
 /// Sugar alcohols, commonly used as sugar substitutes, e.g. erythritol, maltitol, etc.
 ///
 /// **Note**: These are distinct from non-saccharide artificial sweeteners (e.g. aspartame,