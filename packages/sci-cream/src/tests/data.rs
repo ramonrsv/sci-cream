f928889a50aeb7934c64fccdf4a6bd930f080c6f
Ramon Sibello <ramon@sibello.ca>
2026-01-05 15:16:01 -0500

    Improve error messages in `tests:data` module

--- packages/sci-cream/src/tests/data.rs
+++ packages/sci-cream/src/tests/data.rs
@@ -3,17 +3,18 @@ use std::sync::LazyLock;
 
 use crate::specs::IngredientSpec;
 
-pub(crate) fn read_ingredients_file_as_string(filename: &str) -> String {
-    std::fs::read_to_string(format!("./data/ingredients/{filename}")).unwrap()
+pub(crate) fn read_ingredients_file_as_string(filename: &str) -> Result<String, std::io::Error> {
+    std::fs::read_to_string(format!("./data/ingredients/{filename}"))
 }
 
-pub(crate) fn parse_ingredient_specs_from_file(filename: &str) -> HashMap<String, IngredientSpec> {
-    let specs = serde_json::from_str::<Vec<serde_json::Value>>(&read_ingredients_file_as_string(filename)).unwrap();
+pub(crate) fn parse_ingredient_specs_from_string(
+    file_content: &str,
+) -> Result<HashMap<String, IngredientSpec>, serde_json::Error> {
+    let specs = serde_json::from_str::<Vec<serde_json::Value>>(file_content)?;
 
     specs
         .into_iter()
-        .map(|spec| serde_json::from_value(spec).unwrap())
-        .map(|spec: IngredientSpec| (spec.name.clone(), spec))
+        .map(|spec_serde| serde_json::from_value(spec_serde).map(|spec: IngredientSpec| (spec.name.clone(), spec)))
         .collect()
 }
 
@@ -33,7 +34,13 @@ pub(crate) fn get_ingredient_spec_by_name(name: &str) -> Option<IngredientSpec> 
     static ALL_INGREDIENT_SPECS: LazyLock<HashMap<String, IngredientSpec>> = LazyLock::new(|| {
         let mut specs = HashMap::new();
         for filename in INGREDIENT_JSON_FILENAMES {
-            specs.extend(parse_ingredient_specs_from_file(filename));
+            let file_content = read_ingredients_file_as_string(filename)
+                .unwrap_or_else(|e| panic!("Failed to read file '{filename}': {e}"));
+
+            specs.extend(
+                parse_ingredient_specs_from_string(&file_content)
+                    .unwrap_or_else(|e| panic!("Failed to parse ingredient specs from file '{filename}': {e}")),
+            );
         }
         specs
     });
@@ -54,7 +61,8 @@ pub(crate) mod tests {
     #[test]
     fn parse_ingredient_specs() {
         for filename in INGREDIENT_JSON_FILENAMES {
-            let specs = parse_ingredient_specs_from_file(filename);
+            let file_content = read_ingredients_file_as_string(filename).unwrap();
+            let specs = parse_ingredient_specs_from_string(&file_content).unwrap();
             assert_false!(specs.is_empty(), "Failed to parse ingredient specs from file: {}", filename);
         }
     }
