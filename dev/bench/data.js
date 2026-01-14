26731808b9a5185ffa960abb28b913239051f1db
github-action-benchmark <github@users.noreply.github.com>
2026-01-14 07:16:37 +0000

    add sci-cream Rust benchmarks (cargo) benchmark result for 2767da249f2159600b222f1e745b0330b0fbf6c4

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1768374997521,
+  "repoUrl": "https://github.com/ramonrsv/sci-cream",
+  "entries": {
+    "sci-cream Rust benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "ramon@sibello.ca",
+            "name": "Ramon Sibello",
+            "username": "ramonrsv"
+          },
+          "committer": {
+            "email": "ramon@sibello.ca",
+            "name": "Ramon Sibello",
+            "username": "ramonrsv"
+          },
+          "distinct": true,
+          "id": "2767da249f2159600b222f1e745b0330b0fbf6c4",
+          "message": "Give benchmark CI jobs write permission to repo",
+          "timestamp": "2026-01-14T02:15:00-05:00",
+          "tree_id": "b11067bb2b3601e9587b2dda86de2af943b1874d",
+          "url": "https://github.com/ramonrsv/sci-cream/commit/2767da249f2159600b222f1e745b0330b0fbf6c4"
+        },
+        "date": 1768374997218,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "sweetener_spec_into_composition",
+            "value": 314,
+            "range": "± 11",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
