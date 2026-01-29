bf9f5b0812b8ec1143a9335aee5d94e404346ded
github-action-benchmark <github@users.noreply.github.com>
2026-01-29 22:32:12 +0000

    add sci-cream Rust benchmarks (cargo) benchmark result for a95abf2a67f4c74f5b7fbe7b0300d95fdfc0d89f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1769725922443,
+  "lastUpdate": 1769725932974,
   "repoUrl": "https://github.com/ramonrsv/sci-cream",
   "entries": {
     "sci-cream Rust benchmarks": [
@@ -1420,6 +1420,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "sweetener_spec_into_composition",
             "value": 313,
+            "range": "± 3",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ramon@sibello.ca",
+            "name": "Ramon Sibello",
+            "username": "ramonrsv"
+          },
+          "committer": {
+            "email": "ramon@sibello.ca",
+            "name": "Ramon Sibello",
+            "username": "ramonrsv"
+          },
+          "distinct": true,
+          "id": "a95abf2a67f4c74f5b7fbe7b0300d95fdfc0d89f",
+          "message": "Introduce Comp/Mix key get optimizations and bench\n\nIntroduce `Composition` and `MixProperties` get-by-key optimizations,\ncreating JS maps with all the key values and providing helper functions\nto access these instead of via the WASM functions of `Composition` and\n`MixProperties`. Benchmarks show that this is more performant for key\nlookups. However, the maps have to be built every time that a\n`Composition` or `MixProperties` is updated, which could be every\nrender, so this may not be more performant in real usage scenarios.",
+          "timestamp": "2026-01-29T17:11:35-05:00",
+          "tree_id": "584d45b421bd60740f6f0fe306f75dc0c857ae22",
+          "url": "https://github.com/ramonrsv/sci-cream/commit/a95abf2a67f4c74f5b7fbe7b0300d95fdfc0d89f"
+        },
+        "date": 1769725932377,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bridge.calculate_recipe_composition",
+            "value": 3626,
+            "range": "± 69",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bridge.calculate_recipe_mix_properties",
+            "value": 161147,
+            "range": "± 11465",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "recipe.calculate_composition",
+            "value": 2028,
+            "range": "± 40",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "recipe.calculate_mix_properties",
+            "value": 161249,
+            "range": "± 799",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sweetener_spec_into_composition",
+            "value": 312,
             "range": "± 3",
             "unit": "ns/iter"
           }
