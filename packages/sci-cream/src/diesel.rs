48ad75bac035ef4f321fb3670c5de9ac4bc889d9
Ramon Sibello <ramon@sibello.ca>
2026-02-04 15:16:20 -0500

    Disable `missing_docs` for WIP diesel feature/mod

--- packages/sci-cream/src/diesel.rs
+++ packages/sci-cream/src/diesel.rs
@@ -1,5 +1,10 @@
+//! Diesel schema for the `ingredients` table.
+//!
+//! This module is only compiled when the `diesel` feature is enabled.
+
 #[cfg(feature = "diesel")]
 diesel::table! {
+    #[allow(missing_docs)] // WIP, @todo
     ingredients (id) {
         id -> Int4,
         name -> Varchar,
