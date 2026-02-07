e40869e5e1d20ca6255d52a2b5a91c6b76a33fc6
github-action-benchmark <github@users.noreply.github.com>
2026-02-07 06:47:10 +0000

    add App UI Benchmarks (customSmallerIsBetter) benchmark result for 513d2154d9341e03d5ef4d18a53525021f221335

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770446826624,
+  "lastUpdate": 1770446830247,
   "repoUrl": "https://github.com/ramonrsv/sci-cream",
   "entries": {
     "sci-cream Rust benchmarks": [
@@ -32931,6 +32931,90 @@ window.BENCHMARK_DATA = {
             "name": "Rapid ingredient quantity updates, final",
             "value": 30.91,
             "range": "0.59",
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ramon@sibello.ca",
+            "name": "Ramon Sibello",
+            "username": "ramonrsv"
+          },
+          "committer": {
+            "email": "ramon@sibello.ca",
+            "name": "Ramon Sibello",
+            "username": "ramonrsv"
+          },
+          "distinct": true,
+          "id": "513d2154d9341e03d5ef4d18a53525021f221335",
+          "message": "Give semantic names to color patterns and use them\n\nGive semantic names to all color patterns used in globals.css and use\nthem instead of default TailwindCSS. This should make it easier to\nsemantically and consistently apply color themes to all components.",
+          "timestamp": "2026-02-06T17:23:33-05:00",
+          "tree_id": "66c4aa5e7066e349d9613e41b3f415a00d30f6c2",
+          "url": "https://github.com/ramonrsv/sci-cream/commit/513d2154d9341e03d5ef4d18a53525021f221335"
+        },
+        "date": 1770446829330,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage during typical ops",
+            "value": 22.03,
+            "range": "0.000",
+            "unit": "MB"
+          },
+          {
+            "name": "Initial page load",
+            "value": 683.9,
+            "range": "49.46",
+            "unit": "ms"
+          },
+          {
+            "name": "Ingredient name input",
+            "value": 51.7,
+            "range": "17.80",
+            "unit": "ms"
+          },
+          {
+            "name": "Ingredient name input to composition",
+            "value": 61.1,
+            "range": "7.42",
+            "unit": "ms"
+          },
+          {
+            "name": "Ingredient quantity input",
+            "value": 41.5,
+            "range": "8.78",
+            "unit": "ms"
+          },
+          {
+            "name": "Ingredient quantity input to mix property",
+            "value": 38.1,
+            "range": "3.27",
+            "unit": "ms"
+          },
+          {
+            "name": "Recipe paste",
+            "value": 173,
+            "range": "11.46",
+            "unit": "ms"
+          },
+          {
+            "name": "Recipe switch",
+            "value": 50.1,
+            "range": "4.06",
+            "unit": "ms"
+          },
+          {
+            "name": "Rapid ingredient quantity updates, each",
+            "value": 54.35,
+            "range": "0.88",
+            "unit": "ms"
+          },
+          {
+            "name": "Rapid ingredient quantity updates, final",
+            "value": 32.51,
+            "range": "0.71",
             "unit": "ms"
           }
         ]
