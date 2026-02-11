74b9aeb525aaca8b665c85648dc0065417c0fcce
github-action-benchmark <github@users.noreply.github.com>
2026-02-11 06:04:19 +0000

    add sci-cream Rust benchmarks (cargo) benchmark result for 667f20f794471052b8c5683096873348a6e3af06

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770789836409,
+  "lastUpdate": 1770789859312,
   "repoUrl": "https://github.com/ramonrsv/sci-cream",
   "entries": {
     "sci-cream Rust benchmarks": [
@@ -4498,6 +4498,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "sweetener_spec_into_composition",
             "value": 310,
+            "range": "± 2",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ramon@sibello.ca",
+            "name": "Ramon Sibello",
+            "username": "ramonrsv"
+          },
+          "committer": {
+            "email": "ramon@sibello.ca",
+            "name": "Ramon Sibello",
+            "username": "ramonrsv"
+          },
+          "distinct": true,
+          "id": "667f20f794471052b8c5683096873348a6e3af06",
+          "message": "Overhaul `KeySelection`, better popup and Custom\n\n* Overhaul the implementation of `KeySelection`:\n    - Making it a fixed height and scrollable\n    - Adding a 'X' close button\n    - Adding a header 'All Properties' checkbox\n\n* Add an appearing gear icon button used to configure the custom\n  properties, rather than the select menu popping up every time Custom\n  is selected (this also had a problem that it would only pop up when\n  changing from something else to Custom, but not when clicking on\n  Custom if already selected)\n\n* Introduce `qtyToggleToShortStr` to get a shortened version of the\n  string represetnation, namely 'Comp.', 'Qty (g)', and 'Qty (%)'.\n\n* Rework `popupPosition` to use the `top` and `right` values of the\n  bounding rectangle, obviating the need for magic offset values.",
+          "timestamp": "2026-02-10T17:49:30-05:00",
+          "tree_id": "353cff4d54d608637d14aaec7b418b5d5b634e6a",
+          "url": "https://github.com/ramonrsv/sci-cream/commit/667f20f794471052b8c5683096873348a6e3af06"
+        },
+        "date": 1770789858295,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bridge.calculate_recipe_composition",
+            "value": 3572,
+            "range": "± 41",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bridge.calculate_recipe_mix_properties",
+            "value": 162030,
+            "range": "± 963",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "recipe.calculate_composition",
+            "value": 1964,
+            "range": "± 11",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "recipe.calculate_mix_properties",
+            "value": 159942,
+            "range": "± 1012",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sweetener_spec_into_composition",
+            "value": 314,
             "range": "± 2",
             "unit": "ns/iter"
           }
