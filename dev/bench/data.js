eb0c09e7e76a75ed19a8f9b00ed3f72f8896722d
github-action-benchmark <github@users.noreply.github.com>
2026-02-19 04:17:08 +0000

    add App JS benchmarks (benchmarkjs) benchmark result for 62fe1cd4b3cb0cc2f4be3d09ba0e419537729acb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771456078369,
+  "lastUpdate": 1771474628674,
   "repoUrl": "https://github.com/ramonrsv/sci-cream",
   "entries": {
     "sci-cream Rust benchmarks": [
@@ -64769,6 +64769,58 @@ window.BENCHMARK_DATA = {
             "range": "±1.04%",
             "unit": "ops/sec",
             "extra": "89 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ramon@sibello.ca",
+            "name": "Ramon Sibello",
+            "username": "ramonrsv"
+          },
+          "committer": {
+            "email": "ramon@sibello.ca",
+            "name": "Ramon Sibello",
+            "username": "ramonrsv"
+          },
+          "distinct": true,
+          "id": "62fe1cd4b3cb0cc2f4be3d09ba0e419537729acb",
+          "message": "Add `install:wasm|deps` and rename to `build:deps`",
+          "timestamp": "2026-02-18T23:02:06-05:00",
+          "tree_id": "03fa627171eb69e210d3763d47357f3f7a887867",
+          "url": "https://github.com/ramonrsv/sci-cream/commit/62fe1cd4b3cb0cc2f4be3d09ba0e419537729acb"
+        },
+        "date": 1771474627099,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "comp_key_as_med_str",
+            "value": 76432,
+            "range": "±0.75%",
+            "unit": "ops/sec",
+            "extra": "91 samples"
+          },
+          {
+            "name": "compKeyAsMedStr",
+            "value": 724454,
+            "range": "±0.59%",
+            "unit": "ops/sec",
+            "extra": "96 samples"
+          },
+          {
+            "name": "prop_key_as_med_str",
+            "value": 10073,
+            "range": "±2.03%",
+            "unit": "ops/sec",
+            "extra": "82 samples"
+          },
+          {
+            "name": "propKeyAsMedStr",
+            "value": 603244,
+            "range": "±0.71%",
+            "unit": "ops/sec",
+            "extra": "95 samples"
           }
         ]
       }
