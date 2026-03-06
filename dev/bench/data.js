396c4536bfb0e41f0f75e902bb9106c118d73426
github-action-benchmark <github@users.noreply.github.com>
2026-03-06 07:29:48 +0000

    add sci-cream Rust benchmarks (cargo) benchmark result for ec4e3eb33d75c5e43debead4f9ebc7d7d7ae8654

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772665416376,
+  "lastUpdate": 1772782187958,
   "repoUrl": "https://github.com/ramonrsv/sci-cream",
   "entries": {
     "sci-cream Rust benchmarks": [
@@ -8224,6 +8224,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "sweetener_spec_into_composition",
             "value": 358,
+            "range": "± 6",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ramon@sibello.ca",
+            "name": "Ramon Sibello",
+            "username": "ramonrsv"
+          },
+          "committer": {
+            "email": "ramon@sibello.ca",
+            "name": "Ramon Sibello",
+            "username": "ramonrsv"
+          },
+          "distinct": true,
+          "id": "ec4e3eb33d75c5e43debead4f9ebc7d7d7ae8654",
+          "message": "Adjust crate codecov path after root `Cargo.toml`\n\nAfter introducing the root-level workspace `Cargo.toml`, the `target`\ndirectory is created at the repo root instead of `packages/sci-cream`,\nwhich causes `llvm-cov report --output-path ./target/...` to fail due\nto a missing `packages/sci-cream/target` directory. Fix by changing the\noutput-path to `../../target/...` pointing to root-level `target` dir.\n\n@note It's unclear why this didn't immediately fail CI and local `act`\ntests, and why it's failing locally now - not sure if CI would fail.",
+          "timestamp": "2026-03-06T02:09:53-05:00",
+          "tree_id": "c98ccb2bde054d98625b17f6f556f52f4ccfada2",
+          "url": "https://github.com/ramonrsv/sci-cream/commit/ec4e3eb33d75c5e43debead4f9ebc7d7d7ae8654"
+        },
+        "date": 1772782186103,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bridge.calculate_recipe_composition",
+            "value": 3554,
+            "range": "± 73",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bridge.calculate_recipe_mix_properties",
+            "value": 166667,
+            "range": "± 2230",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "recipe.calculate_composition",
+            "value": 2003,
+            "range": "± 9",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "recipe.calculate_mix_properties",
+            "value": 182801,
+            "range": "± 21521",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sweetener_spec_into_composition",
+            "value": 302,
             "range": "± 6",
             "unit": "ns/iter"
           }
